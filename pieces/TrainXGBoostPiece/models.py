ed4429687e81798e58730fee4da791515128c7eb
StanoVo <stano.vojtko@gmail.com>
2025-11-11 21:51:40 +0100

    Correction 1

--- pieces/TrainXGBoostPiece/models.py
+++ pieces/TrainXGBoostPiece/models.py
@@ -1,6 +1,3 @@
-from sklearn.metrics import mean_absolute_error, r2_score
-import joblib
-import pandas as pd
 from pydantic import BaseModel
 
 class InputModel(BaseModel):
@@ -12,15 +9,3 @@ class OutputModel(BaseModel):
     message: str
     model_file: str
     training_log: str
-
-def evaluate(model_path, test_data_path):
-    model = joblib.load(model_path)
-    df = pd.read_csv(test_data_path)
-    X_test = df.drop(columns=['PVOUT'])
-    y_test = df['PVOUT']
-    
-    pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, pred)
-    r2 = r2_score(y_test, pred)
-    
-    return {"MAE": mae, "R2": r2}
--- pieces/TrainXGBoostPiece/piece.py
+++ pieces/TrainXGBoostPiece/piece.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import joblib
 import os
+from sklearn.metrics import mean_absolute_error, r2_score
 from xgboost import XGBRegressor
 from models import InputModel, OutputModel
+
+def evaluate(model_path, test_data_path):
+    model = joblib.load(model_path)
+    df = pd.read_csv(test_data_path)
+    X_test = df.drop(columns=['PVOUT'])
+    y_test = df['PVOUT']
+    
+    pred = model.predict(X_test)
+    mae = mean_absolute_error(y_test, pred)
+    r2 = r2_score(y_test, pred)
+    
+    return {"MAE": mae, "R2": r2}
 
 def main(input_model: InputModel) -> OutputModel:
     print(f"[INFO] Training model using data: {input_model.data_path}")
